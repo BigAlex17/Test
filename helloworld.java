fee049a0b5719141b7dac643b213cc9e86434444
BigAlex17 <gchswork@gmail.com>
2014-06-27 10:27:45 -0400

    Hello world
    loaded

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+/** Comment
+ * Displays "Hello World!" to the standard output.
+ */
+
+class HelloWorld {
+
+  public static void main (String args[]) {
+
+    System.out.println("Hello World!");   //Displays the enclosed String on the Screen Console
+
+  }
